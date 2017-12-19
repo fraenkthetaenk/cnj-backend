66daaa64cfb66989c7225e8dcadae1fe3a2a980a
Frank Steiner <fsteiner@hm.edu>
2017-12-19 23:41:03 +0100

    test

--- src/main/java/edu/hm/cs/cnj/cnjbackend/database/DatabaseController.java
+++ src/main/java/edu/hm/cs/cnj/cnjbackend/database/DatabaseController.java
@@ -1,5 +1,9 @@
 package edu.hm.cs.cnj.cnjbackend.database;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +14,33 @@ public class DatabaseController {
         @Autowired
         DatabaseProperties properties;
         
+        @Value("${spring.datasource.url}")
+        private String dbUrl;
+        
+  
+        public String getRecords()
+        {
+            try (Connection connection = properties.dataSource(dbUrl).getConnection()) {
+                Statement stmt = connection.createStatement();
+                stmt.executeUpdate("CREATE TABLE IF NOT EXISTS infos (info timestamp)");
+                stmt.executeUpdate("INSERT INTO info VALUES (now())");
+                ResultSet rs = stmt.executeQuery("SELECT info FROM infos");
+
+                String output = "";
+                while (rs.next()) {
+                  output += "Read from DB: " + rs.getTimestamp("info") + "\n";
+                }
+                return output;
+              } catch (Exception e) {
+                return "error";
+              } 
+            
+        }
         
         @GetMapping
         public Database getDatabase(){
             Database database = new Database();
-            database.setMessage(properties.getRecords());
+            database.setMessage(getRecords());
             return database;
         }
         
--- src/main/java/edu/hm/cs/cnj/cnjbackend/database/DatabaseProperties.java
+++ src/main/java/edu/hm/cs/cnj/cnjbackend/database/DatabaseProperties.java
@@ -1,16 +1,9 @@
 package edu.hm.cs.cnj.cnjbackend.database;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -20,14 +13,8 @@ import com.zaxxer.hikari.HikariDataSource;
 @Component
 public class DatabaseProperties {
 
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
-    
-    @Autowired
-    private DataSource dataSource;
-
     @Bean
-    public DataSource dataSource() throws SQLException {
+    public DataSource dataSource(String dbUrl) throws SQLException {
         if (dbUrl == null || dbUrl.isEmpty()) {
             return new HikariDataSource();
         } else {
@@ -35,25 +22,7 @@ public class DatabaseProperties {
             config.setJdbcUrl(dbUrl);
             return new HikariDataSource(config);
         }
-
     }    
     
-    public String getRecords()
-    {
-        try (Connection connection = dataSource.getConnection()) {
-            Statement stmt = connection.createStatement();
-            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS infos (info timestamp)");
-            stmt.executeUpdate("INSERT INTO info VALUES (now())");
-            ResultSet rs = stmt.executeQuery("SELECT info FROM infos");
 
-            String output = "";
-            while (rs.next()) {
-              output += "Read from DB: " + rs.getTimestamp("tick") + "\n";
-            }
-            return output;
-          } catch (Exception e) {
-            return "error";
-          } 
-        
-    }
 }
