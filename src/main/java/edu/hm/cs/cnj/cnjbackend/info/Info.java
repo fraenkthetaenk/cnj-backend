0a4f0983e17af80e2a53455e2684f700d91a9b23
Frank Steiner <fsteiner@hm.edu>
2017-12-19 22:12:33 +0100

    testing env variable

--- src/main/java/edu/hm/cs/cnj/cnjbackend/info/Info.java
+++ src/main/java/edu/hm/cs/cnj/cnjbackend/info/Info.java
@@ -1,24 +1,29 @@
 
 package edu.hm.cs.cnj.cnjbackend.info;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel("Server-Info")
 public class Info {
-	
-	@ApiModelProperty(example="Alletsuper!", readOnly=true)
-	String message;
 
-	public Info() {
-		// Default constructor.
-	}
+    @ApiModelProperty(example = "Alletsuper!", readOnly = true)
+    String message;
 
-	public String getMessage() {
-		return message;
-	}
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
 
-	public void setMessage(String message) {
-		this.message = message;
-	}
+    public Info() {
+        // Default constructor.
+    }
+
+    public String getMessage() {
+        return message + dbUrl;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
