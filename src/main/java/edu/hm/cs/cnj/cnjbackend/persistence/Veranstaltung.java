27bfd8f5310273444fa27fa3cd3970231e1ac2ec
Frank Steiner <fsteiner@hm.edu>
2017-12-20 23:14:15 +0100

    changed generation type from auto to identity

--- src/main/java/edu/hm/cs/cnj/cnjbackend/persistence/Veranstaltung.java
+++ src/main/java/edu/hm/cs/cnj/cnjbackend/persistence/Veranstaltung.java
@@ -23,7 +23,7 @@ import org.hibernate.validator.constraints.Length;
 public class Veranstaltung {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     @Column(length=140, unique=true)
