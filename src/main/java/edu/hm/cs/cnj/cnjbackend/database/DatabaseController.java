418357e3005df24bbbecd563c7a8222130f3b69a
Frank Steiner <fsteiner@hm.edu>
2017-12-20 18:40:57 +0100

    tryed diff db con

--- src/main/java/edu/hm/cs/cnj/cnjbackend/database/DatabaseController.java
+++ src/main/java/edu/hm/cs/cnj/cnjbackend/database/DatabaseController.java
@@ -4,54 +4,68 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 @RestController
 @RequestMapping("/db")
 public class DatabaseController {
-    
-        @Autowired
-        DatabaseProperties properties;
-        
-  
-        public String getRecords()
-        {
-            Connection con = properties.getConnection();
-            if (con != null)
-            {      
-                try{
-                    
-                
+
+    // @Autowired
+    // DatabaseProperties properties;
+    //
+
+    @Value("${spring.datasource.url}")
+    private String jdbcUrl;
+    //
+    // @Autowired
+    // private DataSource dataSource;
+
+    public String getRecords() {
+
+        DataSource dataSource = null;
+        if (jdbcUrl == null || jdbcUrl.isEmpty()) {
+            dataSource = new HikariDataSource();
+        } else {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(jdbcUrl);
+            dataSource = new HikariDataSource(config);
+        }
+
+       
+
+            try {
+                Connection con = dataSource.getConnection();
                 Statement stmt = con.createStatement();
-                stmt.executeUpdate("CREATE TABLE IF NOT EXISTS infos (info timestamp)");
-                stmt.executeUpdate("INSERT INTO info VALUES (now())");
-                ResultSet rs = stmt.executeQuery("SELECT info FROM infos");
+//                stmt.executeUpdate("CREATE TABLE IF NOT EXISTS infos (info timestamp)");
+//                stmt.executeUpdate("INSERT INTO info VALUES (now())");
+                ResultSet rs = stmt.executeQuery("SELECT info FROM  Veranstaltung");
 
                 String output = "";
                 while (rs.next()) {
-                  output += "Read from DB: " + rs.getTimestamp("info") + "\n";
+                    output += "Read from DB: " + rs.toString() + "\n";
                 }
                 return output;
-                }
-                catch (Exception e) {
-                    return "error on execution";
-                }
-              }   
-            else
-            {
-                return "Error";
+            } catch (Exception e) {
+                return "error on execution" + e.getMessage();
             }
-        }
         
-        @GetMapping
-        public Database getDatabase(){
-            Database database = new Database();
-            database.setMessage(getRecords());
-            return database;
-        }
-        
+    }
+
+    @GetMapping
+    public Database getDatabase() {
+        Database database = new Database();
+        database.setMessage(getRecords());
+        return database;
+    }
+
 }
