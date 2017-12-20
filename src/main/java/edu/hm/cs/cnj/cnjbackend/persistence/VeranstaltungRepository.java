9832b115cde7557601ca59b1d2015c5fd853e6d9
Frank Steiner <fsteiner@hm.edu>
2017-12-20 20:44:49 +0100

    test diff bais

--- src/main/java/edu/hm/cs/cnj/cnjbackend/persistence/VeranstaltungRepository.java
+++ src/main/java/edu/hm/cs/cnj/cnjbackend/persistence/VeranstaltungRepository.java
@@ -3,10 +3,11 @@ package edu.hm.cs.cnj.cnjbackend.persistence;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 
-public interface VeranstaltungRepository extends CrudRepository <Veranstaltung, Long> {
+public interface VeranstaltungRepository extends JpaRepository <Veranstaltung, Long> {
 
     List<Veranstaltung> findByBeginnBefore(Date date);
 
