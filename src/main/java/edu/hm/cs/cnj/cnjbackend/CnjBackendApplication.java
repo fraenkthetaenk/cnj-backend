098d686e102516c26ef00e5b9f5b202f98832d71
Frank Steiner <fsteiner@hm.edu>
2018-01-06 23:37:33 +0100

    changed DB place

--- src/main/java/edu/hm/cs/cnj/cnjbackend/CnjBackendApplication.java
+++ src/main/java/edu/hm/cs/cnj/cnjbackend/CnjBackendApplication.java
@@ -10,11 +10,39 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
 @SpringBootApplication
 @EnableSwagger2
 public class CnjBackendApplication {
+	
+	@Value("${spring.datasource.url}")
+    private String jdbcUrl;
 
 	public static void main(String[] args) {
+
+		DataSource dataSource = null;
+        if (jdbcUrl == null || jdbcUrl.isEmpty()) {
+            dataSource = new HikariDataSource();
+        } else {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(jdbcUrl);
+            dataSource = new HikariDataSource(config);
+        }
+
+ try {
+                Connection con = dataSource.getConnection();
+                Statement stmt = con.createStatement();
+                stmt.executeUpdate("CREATE TABLE IF NOT EXISTS Veranstaltung (id serial, titel varchar(140), beschreibung varchar(1000), beginn date)");
+                stmt.executeUpdate("CREATE TABLE IF NOT EXISTS Teilnahmen (id serial, name varchar(255), begleiter integer, status varchar(20), veranstaltung_id integer)");
+
+            } catch (Exception e) {
+                
+            }
+
 		SpringApplication.run(CnjBackendApplication.class, args);
 	}
 
